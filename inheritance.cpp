c4e3fde953331d8c549185aab14ef2bf5d0b5586
aliyanadaa <aliyanadaq@gmail.com>
2026-05-11 17:25:35 +0700

    menambahkan constructor dan destructor pada class orang

--- inheritance.cpp
+++ inheritance.cpp
@@ -5,6 +5,14 @@ using namespace std;
 class orang {
     public:
         string nama;
+
+        orang(string pNama) : nama(pNama) {
+            cout << "Orang dibuat\n" << endl;
+        }
+
+        ~orang() {
+            cout << "Orang dihapus\n" << endl;
+        }
 };
 
 int main() {
