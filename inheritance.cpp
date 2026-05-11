ff5dd7a6f47af4eebf795d7d9fb0859af9ff2719
aliyanadaa <aliyanadaq@gmail.com>
2026-05-11 17:26:44 +0700

    menambahkan implementasi object pelajar pada main

--- inheritance.cpp
+++ inheritance.cpp
@@ -39,6 +39,13 @@ class pelajar : public orang {
 };
 
 int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+
+    cout << siswa1.perkenalan();
+
+    cout << "Hasil = "
+         << siswa1.jumlah(10, 90)
+         << endl;
 
     return 0;
 }
