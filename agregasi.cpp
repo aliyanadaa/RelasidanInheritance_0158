1d30449537711b7d32b34a8bb372cbe644d31722
aliyanadaa <aliyanadaq@gmail.com>
2026-05-11 17:20:25 +0700

    menambahkan pembuatan objek ibu dan anak

--- agregasi.cpp
+++ agregasi.cpp
@@ -5,6 +5,12 @@ using namespace std;
 #include "anak.h"
 
 int main() {
+    ibu* varIbu = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+
+    anak* varAnak1 = new anak("Tono");
+    anak* varAnak2 = new anak("Rini");
+    anak* varAnak3 = new anak("Dewi");
 
     return 0;
 }
