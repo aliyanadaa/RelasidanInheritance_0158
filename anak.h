d9e8fa4021e6818f4c30b5812e929105ab99e62c
aliyanadaa <aliyanadaq@gmail.com>
2026-05-11 16:03:44 +0700

    menambahkan destructor pada class anak

--- anak.h
+++ anak.h
@@ -11,6 +11,10 @@ class anak {
         anak(string pNama) : nama(pNama) {
             cout << "Anak \"" << nama << "\" ada\n";
         }
+
+        ~anak() {
+            cout << "Anak \"" << nama << "\" tidak ada\n";
+        }
 };
 
 
