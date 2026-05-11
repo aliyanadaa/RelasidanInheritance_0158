a1c92a08cae1e7708190d2340c9caa3af9ef4372
aliyanadaa <aliyanadaq@gmail.com>
2026-05-11 17:22:33 +0700

    menambahkan deklarasi class dokter dan pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -2,6 +2,19 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+class pasien {
+    public:
+        string nama;
+        vector<dokter*> daftar_dokter;
+};
+
+class dokter {
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+};
+
 int main() {
 
     return 0;
