05cdaf2bac5e9787c56c1870d9c3f83d0b2114c0
aliyanadaa <aliyanadaq@gmail.com>
2026-05-11 16:01:30 +0700

    menambahkan header guard pada file anak.h

--- anak.h
+++ anak.h
@@ -1,19 +1,6 @@
 #ifndef ANAK_H
 #define ANAK_H
 
-#include <iostream>
-using namespace std;
 
-class anak {
-    public:
-        string nama;
-        anak(string pNama) :nama(pNama) {
-            cout << "Anak \"" << nama << "\" ada\n";
-        }
-
-        ~anak() {
-            cout << "Anak \"" << nama << "\" tidak ada\n";
-        }
-};
 
 #endif
